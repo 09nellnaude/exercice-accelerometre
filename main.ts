2737505e7c9cdc7356998d0e68c34950a4cfb8bf
09nellnaude <103410355+09nellnaude@users.noreply.github.com>
2022-05-23 09:25:48 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,75 @@
 basic.forever(function () {
-    if (input.acceleration(Dimension.Y) > 100) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
-    } else if (input.acceleration(Dimension.Y) < -100) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-    } else if (input.acceleration(Dimension.X) > -100) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
-    } else if (input.acceleration(Dimension.X) < 100) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
+    if (input.acceleration(Dimension.Y) < -200) {
+        if (input.acceleration(Dimension.X) < -200) {
+            basic.showArrow(ArrowNames.NorthWest)
+        } else if (input.acceleration(Dimension.X) > 200) {
+            basic.showLeds(`
+                . # # # #
+                . . . # #
+                . . # . #
+                . # . . #
+                # . . . .
+                `)
+        } else {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . # . .
+                `)
+        }
+    } else if (input.acceleration(Dimension.Y) > 200) {
+        if (input.acceleration(Dimension.X) < -200) {
+            basic.showLeds(`
+                . . . . #
+                # . . # .
+                # . # . .
+                # # . . .
+                # # # # .
+                `)
+        } else if (input.acceleration(Dimension.X) > 200) {
+            basic.showLeds(`
+                # . . . .
+                . # . . #
+                . . # . #
+                . . . # #
+                . # # # #
+                `)
+        } else {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+        }
     } else {
-        basic.showLeds(`
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            . . . . .
-            `)
+        if (input.acceleration(Dimension.X) < -200) {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+        } else if (input.acceleration(Dimension.X) > 200) {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+        } else {
+            basic.showLeds(`
+                . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                . . . . .
+                `)
+        }
     }
 })
